cdcf26f918ec08828a64d5d201257322c7eee078
Cong Dinh <congdinh2012@hotmail.com>
2023-07-08 02:46:28 +0700

    Update selenium test with logging

--- automatedtesting/selenium/ui-tests.py
+++ automatedtesting/selenium/ui-tests.py
@@ -2,11 +2,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-import datetime
+import logging
 
-def timestamp():
-    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return (ts + '\t')
+logging.basicConfig(
+    filename='selenium.log',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 URL_LOGIN = 'https://www.saucedemo.com/'
 URL_INVENTORY = 'https://www.saucedemo.com/inventory.html'
@@ -16,7 +18,7 @@ URL_CART = 'https://www.saucedemo.com/cart.html'
 
 
 def login(user, password):
-    print(timestamp() + 'Starting the login test')
+    logging.info('Starting the login test')
     # uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--no-sandbox") 
@@ -31,9 +33,10 @@ def login(user, password):
     # On Local
     # driver = webdriver.Chrome()
 
-    print(timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
+    logging.info(
+        'Browser started successfully. Navigating to the demo page to login.')
     driver.get(URL_LOGIN)
-    print(timestamp() + 'Navigating to the demo page to login.')
+    logging.info('Navigating to the demo page to login.')
 
     # login
     driver.find_element(
@@ -47,7 +50,7 @@ def login(user, password):
     product_label = driver.find_element(
         By.CSS_SELECTOR, "div.header_secondary_container > span.title").text
     assert "Products" in product_label
-    print(timestamp() + 'Login successfully as {:s} .'.format(user))
+    logging.info('Login successfully as {:s} .'.format(user))
 
     return driver
 
@@ -55,7 +58,7 @@ def login(user, password):
 
 
 def addItem(driver):
-    print(timestamp() + "Start test adding item to cart")
+    logging.info("Start test adding item to cart")
     items = driver.find_elements(By.CSS_SELECTOR, '.inventory_item')
 
     for i in range(0, len(items)):
@@ -63,7 +66,7 @@ def addItem(driver):
 
         item_name = item.find_element(
             By.CLASS_NAME, 'inventory_item_name').text
-        print(timestamp() + "Add item: {}".format(item_name))
+        logging.info("Add item: {}".format(item_name))
         button = item.find_element(By.CSS_SELECTOR, '.pricebar > button')
         button.click()
 
@@ -71,16 +74,16 @@ def addItem(driver):
         By.CLASS_NAME, 'shopping_cart_badge').text
     testItemAdded = "6"
 
-    print(timestamp() + "Total tested item added to cart: {}".format(testItemAdded))
-    print(timestamp() + "Total item added to cart: {}".format(totalItemOnCartLabel))
+    logging.info("Total tested item added to cart: {}".format(testItemAdded))
+    logging.info("Total item added to cart: {}".format(totalItemOnCartLabel))
     assert testItemAdded == totalItemOnCartLabel, "Total added item on cart not matched"
 
-    print(timestamp() + "All item added to cart successfully")
+    logging.info("All item added to cart successfully")
 
 # Test remove all products to cart
 
 def removeItem(driver):
-    print(timestamp() + "Start test remove item from the cart")
+    logging.info("Start test remove item from the cart")
 
     items = driver.find_elements(By.CSS_SELECTOR, '.cart_item')
 
@@ -89,7 +92,7 @@ def removeItem(driver):
 
         item_name = item.find_element(
             By.CLASS_NAME, 'inventory_item_name').text
-        print(timestamp() + "Remove item: {}".format(item_name))
+        logging.info("Remove item: {}".format(item_name))
         button = item.find_element(By.CSS_SELECTOR, '.item_pricebar > button')
         button.click()
 
@@ -97,12 +100,12 @@ def removeItem(driver):
         By.CLASS_NAME, 'shopping_cart_link').text
     testItemRemove = "6"
 
-    print(timestamp() + 
+    logging.info(
         "Total tested item remove from the to cart: {}".format(testItemRemove))
     assert "" == totalItemOnCartLabel, "Total added item on cart not matched"
 
     # Check on cart url
-    print(timestamp() + "Test removed item on the cart page")
+    logging.info("Test removed item on the cart page")
     driver.get(URL_CART)
     totalItemsOnCart = driver.find_elements(By.CLASS_NAME, 'cart_item')
     numberOfItemsInCart = 0
@@ -110,7 +113,7 @@ def removeItem(driver):
     assert numberOfItemsInCart == len(
         totalItemsOnCart), "Total added item on cart not matched"
 
-    print(timestamp() +  "All item removed from cart successfully")
+    logging.info("All item removed from cart successfully")
 
 
 if __name__ == "__main__":
@@ -118,4 +121,4 @@ if __name__ == "__main__":
     addItem(driver)
     removeItem(driver)
 
-    print(timestamp() + "UI Tests are successfully completed")
+    logging.info("UI Tests are successfully completed")
